ad321a7e26a4afdd7e3a8139e91e87facf2f3595
zzh <396559551@qq.com>
2021-08-09 20:36:58 +0800

    完成图表

--- src/router/index.js
+++ src/router/index.js
@@ -65,6 +65,11 @@ const routes = [
         name: 'Order',
         component: () => import('../components/order/Order')
       },
+      {
+        path: '/reports',
+        name: 'Report',
+        component: () => import('../components/report/Report')
+      },
     ]
   },
 
